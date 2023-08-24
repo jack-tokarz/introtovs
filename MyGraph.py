98181b3ddc32db5cb1eab7eda8901f6e9ed69cea
jack_tokarz1@baylor.edu <jack_tokarz1@baylor.edu>
2023-08-24 15:22:40 -0500

    initial commit

---
+++ MyGraph.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0,20,100)
+plt.plot(x, np.sin(x))
+plt.show()
+
+#Quiz 1 (I took pictures)
+    #step one: create a VE
+    #Step two: Activate the VE
+    #Step three: Install 3rd party libaries
+
+
